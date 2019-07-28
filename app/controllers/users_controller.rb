ba2fe0e526cec35f1f72d7da5ca8f52d930c80cc
tama-7010 <otama.engine@gmail.com>
2019-07-28 22:13:03 +0900

    Add signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,11 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcom to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
